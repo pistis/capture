8e32cca2e84aee811422cf4eb7aa6689003a0b8e
pistis <vamalboro@gmail.com>
2018-09-19 22:34:40 +0900

    Improved quality of captured images.

--- study/renderer/selector/capturer.js
+++ study/renderer/selector/capturer.js
@@ -1,22 +1,23 @@
 const { desktopCapturer } = require('electron');
 
-const captureScreen = (displayId) => {
+const captureScreen = (displayId, width, height) => {
   return new Promise((resolve, reject) => {
     desktopCapturer.getSources({ types: ['screen'] }, (error, sources) => {
       if (error) throw error;
       for (let i = 0; i < sources.length; ++i) {
         if (sources[i].id === `screen:${displayId}`) {
           navigator.webkitGetUserMedia(
+            // https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
             {
               audio: false,
               video: {
                 mandatory: {
                   chromeMediaSource: 'desktop',
                   chromeMediaSourceId: sources[i].id,
-                  minWidth: 1280,
-                  maxWidth: 4000,
-                  minHeight: 720,
-                  maxHeight: 4000,
+                  minWidth: width,
+                  maxWidth: width * 2,
+                  minHeight: height,
+                  maxHeight: height * 2,
                 },
               },
             },
--- study/renderer/selector/default-test.js
+++ study/renderer/selector/default-test.js
@@ -1,49 +1,10 @@
 const electron = require('electron');
-const Jimp = require('jimp');
 const { captureScreen } = require('./capturer');
 const { ipcRenderer } = electron;
 
-const previewCapture = (base64data, rect) => {
-  const { screen } = electron;
-  // add to buffer base64 image instead of saving locally in order to manipulate with Jimp
-  const encondedImageBuffer = new Buffer(
-    base64data.replace(/^data:image\/(png|gif|jpeg);base64,/, ''),
-    'base64'
-  );
-
-  const height = rect.height;
-  const width = rect.width;
-  const distanceX = rect.x;
-  const distanceY = rect.y;
-  const screenDimensions = screen.getPrimaryDisplay().size;
-  const screenHeight = screenDimensions.height;
-  const screenWidth = screenDimensions.width;
-
-  Jimp.read(encondedImageBuffer, function(err, image) {
-    if (err) throw err;
-
-    // Show the original width and height of the image in the console
-    console.log(image.bitmap.width, image.bitmap.height);
-
-    // Resize the image to the size of the screen
-    image.resize(screenWidth, screenHeight);
-    // Crop image according to the coordinates
-    // add some margin pixels for this example
-    image
-      .crop(distanceX, distanceY, width, height)
-      // Get data in base64 and show in img tag
-      .getBase64('image/jpeg', function(err, base64data) {
-        const preview = document.getElementById('preview');
-        preview.setAttribute('src', base64data);
-        preview.style.cssText = `width:${rect.width}px;height:${
-          rect.height
-        }px;`;
-        //console.log(data);
-      });
-  });
-};
 ipcRenderer.on('display', (event, displayInfo) => {
   console.log(`displayInfo ${JSON.stringify(displayInfo)}`); // TODO : can use displayInfo later.
+
   captureScreen(displayInfo.id, displayInfo.width, displayInfo.height)
     .then((stream) => {
       console.log('captured screen', stream);
@@ -65,12 +26,9 @@ ipcRenderer.on('display', (event, displayInfo) => {
         // Draw video on canvas
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-        previewCapture(canvas.toDataURL(imageFormat), {
-          x: 0,
-          y: 0,
-          width: 1024,
-          height: 768,
-        });
+        document
+          .getElementById('preview')
+          .setAttribute('src', canvas.toDataURL(imageFormat)); // TODO: 퀄리티가 jimp 사용시보다 좋다.
 
         // Remove hidden video tag
         video.remove();
@@ -90,10 +48,3 @@ ipcRenderer.on('display', (event, displayInfo) => {
       //  만약 사용자가 권한요청에 거부하거나 사용할 수 있는 미디어 장치가 없다면 promise는 rejected 상태로 PermissionDeniedError 또는 NotFoundError 를 반환합니다.
     });
 });
-
-// ipcRenderer.on('captured-screen', (event, stream) => {
-//   console.log('captured-screen', stream);
-// });
-// window.addEventListener('load', () => {
-//   ipcRenderer.send('capture-screen');
-// });
