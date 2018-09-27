9a9a7fd0827aa9fe789359f811e42fc7acd7b3bf
pistis <vamalboro@gmail.com>
2018-09-27 23:27:20 +0900

    remove ipcMain in main.js

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-const { app, globalShortcut, ipcMain } = require('electron');
+const { app, globalShortcut } = require('electron');
 const { openCaptureWindow } = require('./capturer');
 
 app.on('ready', async () => {
